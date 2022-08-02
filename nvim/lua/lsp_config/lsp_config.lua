0ce78ec2beff405ed06aa069ec98a2bda9edb2ce
garikmi <imgarik@icloud.com>
2022-08-02 15:42:11 -0700

    Setup lua server

--- nvim/lua/lsp_config/lsp_config.lua
+++ nvim/lua/lsp_config/lsp_config.lua
@@ -39,6 +39,7 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
+-- Setup servers
 require('lspconfig')['jdtls'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
@@ -78,5 +79,30 @@ require('lspconfig')['marksman'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
+}
+require('lspconfig')['sumneko_lua'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 
